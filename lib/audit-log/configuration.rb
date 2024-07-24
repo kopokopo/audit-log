567f60eed27e15270c157f0490ea1c5d77f0c3e5
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2024-07-24 11:24:26 +0300

    KK-7245: Add two auditable identifiers to associate to audit log events.

--- lib/audit-log/configuration.rb
+++ lib/audit-log/configuration.rb
@@ -7,5 +7,8 @@ module AuditLog
 
     # set a special table_name for AuditLog Model, default: 'audit_logs'
     attr_accessor :table_name
+
+    #auditable identifiers methods, defaults: "auditable_identifier_one" and "auditable_identifier_two respectively"
+    attr_accessor :identifier_one_method, :identifier_two_method
   end
 end
