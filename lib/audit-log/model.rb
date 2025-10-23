69903c607bbdfcc51a4875b5baf5bad722670aa9
gillerick <ogayogill95@gmail.com>
2025-10-23 11:02:23 +0300

    fix(KK-11019): Mask sensitive information on payload

--- lib/audit-log/model.rb
+++ lib/audit-log/model.rb
@@ -4,6 +4,8 @@ module AuditLog
   module Model
     extend ActiveSupport::Concern
 
+    SENSITIVE_KEYS = %w[pin password secret].freeze
+
     included do
       self.table_name = AuditLog.config.table_name
 
@@ -17,11 +19,17 @@ module AuditLog
       validates :action, presence: true
 
       after_initialize :initialize_payload_request
+      before_save :mask_payload_and_request
     end
 
     def initialize_payload_request
       self.payload ||= {}
       self.request ||= {}
+    end
+
+    def mask_payload_and_request
+      self.payload = mask_sensitive_data(self.payload)
+      self.request = mask_sensitive_data(self.request)
     end
 
     def user_name
@@ -32,5 +40,25 @@ module AuditLog
     def action_name
       I18n.t("audit_log.action.#{action}", default: action)
     end
+
+    private
+
+    def mask_sensitive_data(payload = {})
+      case payload
+      when Hash
+        payload.transform_keys(&:to_s).to_h do |key, value|
+          if SENSITIVE_KEYS.any? { |word| key.downcase.include?(word) }
+            [key, '*****']
+          else
+            [key, mask_sensitive_data(value)]
+          end
+        end
+      when Array
+        payload.map { |v| mask_sensitive_data(v) }
+      else
+        payload
+      end
+    end
+
   end
 end
